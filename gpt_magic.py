5d356bc72f3d414c0cc7cd601d78e0ea6b92c13b
aleksandr88p <aleksandr88p@gmail.com>
2023-06-07 00:06:05 +0200

    fix promt

--- gpt_magic.py
+++ gpt_magic.py
@@ -12,16 +12,19 @@ def generate_summary(api_key, article_groups, query=None):
         shortest_article = min(group_articles, key=lambda x: len(x[1]))
         article_to_summarize, link = shortest_article[1], shortest_article[0]
 
-        default_query = "Summarize the following text in 3-8 sentences written a conversational, direct, casual, accessible, chatty and down-to-earth, but catered to insider industry professionals. Give it a catchy title (not in quotations) that includes important keywords and relevant emojis. Bold the most important phrases or key takeaways, write all important names in uppercase, and include emojis where relevant"
-        query = query if query else default_query
-        query += ': \n\n'
-        prompt = query + article_to_summarize
+        default_query = "Summarize the following text in 3-8 sentences written a conversational, direct, casual, accessible, chatty and down-to-earth, but catered to insider industry professionals. Give it a catchy title (not in quotations) that includes important keywords and relevant emojis. Bold the most important phrases or key takeaways, write all important names in uppercase, and include emojis where relevant: \n\n"
+        # query = f"{query}: \n\n" if query else default_query
+        # query += ''
+        # prompt = query + article_to_summarize
+        full_query = f"{query}: \n\n" if query else default_query
+        full_query += ''
+        prompt = full_query + article_to_summarize
         @retry(stop_max_attempt_number=3, wait_fixed=30 * 1000)  # Retry 3 times with a 30-second wait between attempts
         def request_summary():
             try:
-
+                print('***************************************')
                 print(f'from try, len promt is {len(prompt)}')
-
+                print(prompt)
                 response = openai.Completion.create(
                     engine="text-davinci-003",
                     prompt=prompt,
@@ -39,6 +42,6 @@ def generate_summary(api_key, article_groups, query=None):
             summary = article_to_summarize  # Fall back to the original article if no summary was generated
 
         summaries[group_key] = summary
-        print(f"Summary generated for {group_key} based on the article: {link}")
+        print(f"\nSummary generated for {group_key} based on the article: {link}\n")
 
     return summaries
